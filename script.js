0414f81219ffa6712f6090cf29379f4e0f94ad0d
Sly Foxer <jdavidbx@gmail.com>
2020-10-27 00:14:20 -0400

    Add random number generator variable

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 const neverArray = ['back down', 'give up', 'quit', 'stop trying', "think you can't"];
 const alwaysARray = ['give it your all', 'know you can do it', 'believe in yourself', 'put your best foot forward', 'stay positive'];
 const motivateArray = ['You can do it', "You're the best", "There's nothing you can't do", 'Keep going', "You'll make your dreams a reality"];
+
+//Random number generator
+const randomNumber = Math.floor(Math.random() * 5);
