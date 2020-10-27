600b3ceb963e46cc30c92b9c1eb062b8fe384462
Sly Foxer <jdavidbx@gmail.com>
2020-10-27 00:11:37 -0400

    Fix typo in third array variable name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //Message component arrays
 const neverArray = ['back down', 'give up', 'quit', 'stop trying', "think you can't"];
 const alwaysARray = ['give it your all', 'know you can do it', 'believe in yourself', 'put your best foot forward', 'stay positive'];
-const motivateARray = ['You can do it', "You're the best", "There's nothing you can't do", 'Keep going', "You'll make your dreams a reality"];
+const motivateArray = ['You can do it', "You're the best", "There's nothing you can't do", 'Keep going', "You'll make your dreams a reality"];
